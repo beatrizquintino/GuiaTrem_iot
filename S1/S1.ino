16b9c9d8e0d448933c06b46ebf16f915ca4d44b8
beatrizquintino <beatriz_quintino@estudante.sesisenai.org.br>
2025-09-30 12:47:35 -0300

    Código WiFi

--- S1/S1.ino
+++ S1/S1.ino
@@ -1,7 +1,17 @@
-void setup() {
+#include <WiFi.h>
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
 
+void setup() {
+  Serial.begin(115200);    //configura a placa para mostrar na tela
+  WiFi.begin(SSID, PASS);  // tenta conectar na rede
+  Serial.println("Conectando no Wifi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("Conectado com sucesso");
 }
 
 void loop() {
-  
 }
