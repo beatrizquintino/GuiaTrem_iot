6bc400466ec48d331a6ec2308375cfbe7e89ff66
Hyago-Mariano <hyago_m_alves@estudante.sesisenai.org.br>
2025-09-30 12:48:46 -0300

    Conexão com WiFi básica

--- S_n_Hyago/S_n_Hyago.ino
+++ S_n_Hyago/S_n_Hyago.ino
@@ -1,8 +1,16 @@
-
+#include<WiFi.h>
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
 
 void setup() {
-
-
+  Serial.begin(115200); //configura a plca para mostrar na tela
+  WiFi.begin(SSID, PASS); //tenta conectar na rede
+  Serial.println("conectando no wifi");
+  while(WiFi.status() !=WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("conectado com sucesso!");
 }
 
 void loop() {
