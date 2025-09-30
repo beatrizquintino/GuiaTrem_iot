3e6996b73da1a4f2d5aeaa4082725b39d7e9db19
Bruno-H-Correa <bruno_h_correa@estudante.sesisenai.org.br>
2025-09-30 12:49:58 -0300

    Conexão com WiFi basica

--- SN_Bruno/SN_Bruno.ino
+++ SN_Bruno/SN_Bruno.ino
@@ -1,6 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
+const String SSID = "nome da rede";
+const String PASS = "senha da rede";
 
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(SSID, PASS);
+  Serial.println("Conectando no WiFi");
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("Conectado com sucesso");
 }
 
 void loop() {
