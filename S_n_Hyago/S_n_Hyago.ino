eeba8fc0f18451234ba553feb108ac07466c457a
Hyago-Mariano <hyago_m_alves@estudante.sesisenai.org.br>
2025-10-07 12:36:22 -0300

    conexao com o broker

--- S_n_Hyago/S_n_Hyago.ino
+++ S_n_Hyago/S_n_Hyago.ino
@@ -1,19 +1,43 @@
-#include<WiFi.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+WiFiClient client;          //cria objeto p/ WiFi
+PubSubClient mqtt(client);  //cria objeto p/ mqtt usando WiFi
+
+const String brokerURL = "test.mosquitto.org";
+const int brokerPort = 1883;
+
+const String brokerUser = "";  //variável para o user do brocker
+const String brokerPass = "";  //variável para a senha do brocker
+
+
+
 const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
 
 void setup() {
-  Serial.begin(115200); //configura a plca para mostrar na tela
-  WiFi.begin(SSID, PASS); //tenta conectar na rede
-  Serial.println("conectando no wifi");
-  while(WiFi.status() !=WL_CONNECTED){
+  Serial.begin(115200);    //configura a placa para mostrar na tela
+  WiFi.begin(SSID, PASS);  // tenta conectar na rede
+  Serial.println("Conectando no WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(200);
   }
-  Serial.println("conectado com sucesso!");
+  Serial.println("\nConectado com sucesso!");
+
+  //Configura a placa para mostra na tela
+  mqtt.setServer(brokerURL.c_str(), brokerPort);
+  Serial.println("Conectando no Broker");
+  
+  String boardID = "S1-"; //Cria um nome que começa com "s1-"
+  boardID += String(random(0xffff),HEX); //Junta o "s1-" com um número aleatório Hexadecimal
+
+  //Enquanto não estiver conectado mostra "."
+  while (!mqtt.connect(boardID.c_str())){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("\nConectado com sucesso ao broker");
 }
 
-void loop() {
-
-
-}
+void loop() {}
