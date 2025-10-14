85b83688d72c241846d5403370d2acce04b11c5d
HYAGO MARIANO DOS SANTOS ALVES <hyago_m_alves@estudante.sesisenai.org.br>
2025-10-14 12:52:59 -0300

    acender led vitor -> hyago

--- S_n_Hyago/S_n_Hyago.ino
+++ S_n_Hyago/S_n_Hyago.ino
@@ -16,7 +16,98 @@ const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
 
 void setup() {
+  Serial.begin(115200);    /#include <WiFi.h>
+#include <PubSubClient.h>
+
+WiFiClient client;          //cria objeto p/ WiFi
+PubSubClient mqtt(client);  //cria objeto p/ mqtt usando WiFi
+
+const byte ledPin = 2;
+
+const String brokerURL = "test.mosquitto.org";
+const int brokerPort = 1883;
+const String topico = "topicoHY"; 
+
+const String brokerUser = "";  //variável para o user do brocker
+const String brokerPass = "";  //variável para a senha do brocker
+
+
+
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
   Serial.begin(115200);    //configura a placa para mostrar na tela
+  WiFi.begin(SSID, PASS);  // tenta conectar na rede
+  Serial.println("Conectando no WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(200);
+  }
+
+  Serial.println("\nConectado com sucesso!");
+
+
+
+  //Configura a placa para mostra na tela
+  mqtt.setServer(brokerURL.c_str(), brokerPort);
+  Serial.println("Conectando no Broker");
+  
+  String boardID = "S1-"; //Cria um nome que começa com "s1-"
+  boardID += String(random(0xffff),HEX); //Junta o "s1-" com um número aleatório Hexadecimal
+
+  //Enquanto não estiver conectado mostra "."
+  while (!mqtt.connect(boardID.c_str())){
+    Serial.print(".");
+    delay(200);
+  }
+  mqtt.subscribe(topico.c_str()); //vamos mudar (topico)
+  mqtt.setCallback(callback);
+  Serial.println("\nConectado com sucesso ao broker");
+}
+
+void loop() {
+  /*String msg = "TESTE"; //Informação que será enviada para o broker
+  String topico = "AulaIoT/msg";
+  mqtt.publish(topico.c_str(), msg.c_str());
+  delay(1000);
+  Serial.println(msg);
+  mqtt.loop();*/
+ 
+
+ //envia a mensagem para um lugar
+  String msg = "";
+  if(Serial.available() > 0 ){
+    msg = Serial.readStringUntil('\n'); //avalia as letras escritas ate encontrar \n assim colocando dentro de 'mensagem'
+    Serial.print("mensagem digitada: ");
+    Serial.println(msg);
+    
+    mqtt.publish("TopicoVitor", msg.c_str()); // envia a mensagem 
+  }
+  mqtt.loop();
+}
+
+void callback(char* topic, byte* payload, unsigned long length){
+  String msgRecebida = "";
+  for(int i = 0; i< length; i++){
+    msgRecebida += (char) payload[i];
+  }
+  Serial.println(msgRecebida);
+  //Fazer o controle aqui
+
+  if(msgRecebida == "acender" ) {
+    digitalWrite(ledPin, HIGH); 
+    delay(1000);      
+    Serial.println("ligando...");         
+  }
+
+  if(msgRecebida == "apagar" ) {              
+    digitalWrite(ledPin, LOW);  
+    delay(1000); 
+    Serial.println("apagando...");
+  }
+}/configura a placa para mostrar na tela
   WiFi.begin(SSID, PASS);  // tenta conectar na rede
   Serial.println("Conectando no WiFi");
   while (WiFi.status() != WL_CONNECTED) {
