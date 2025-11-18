8956ececae4d0258a8fac19dec0de652dedbc25c
BEATRIZ SARAMENTO QUINTINO <beatriz_quintino@estudante.sesisenai.org.br>
2025-11-18 12:46:06 -0300

    Código do S1 finalizado

--- S1/S1.ino
+++ S1/S1.ino
@@ -1,20 +1,28 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <WiFiClientSecure.h>
 #include "env.h"
 
-WiFiClient client;          //cria objeto p/ WiFi
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+WiFiClientSecure client;          //cria objeto p/ WiFi
 PubSubClient mqtt(client);  //cria objeto p/ mqtt usando WiFi
 
-const String brokerURL = "test.mosquitto.org";
-const int brokerPort = 1883;
-const String topico = "TopicoBeatriz";  //nome do topico
-const String brokerUser = "";           //variável para o user do brocker
-const String brokerPass = "";           //variável para a senha do brocker
+const byte trigg_pin = 22;
+const byte echo_pin = 23;
+const int ledPin = 19;
+const int pino_LDR = 34;
 
-const int ledPin = 2;
+DHT dht(DHTPIN, DHTTYPE);
+
+int distancia = 0;
+unsigned long tempo = 0;
+float temperatura = 0;
+float umidade = 0;
+
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
   Serial.begin(115200);    //configura a placa para mostrar na tela
   WiFi.begin(SSId, PASS);  // tenta conectar na rede
   Serial.println("Conectando no WiFi");
@@ -41,30 +49,63 @@ void setup() {
   mqtt.subscribe(TOPIC1);
   mqtt.setCallback(callback);
   Serial.println("\nConectado com sucesso ao broker");
+
+  //DHT
+  sensorDHT.begin();
+
+  //presença
+  pinMode(trigg_pin, OUTPUT);
+  pinMode(echo_pin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+long lerDistancia() {
+  digitalWrite(trigg_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigg_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigg_pin, LOW);
+  
+  long duracao = pulseIn(echo_pin, HIGH);
+  long distancia = duracao * 349.24 / 2 / 10000;
+  
+  return distancia;
 }
 
 void loop() {
-  String msg = "";  //Informação que será enviada para o broker
-  //String topico = "AulaIoT/msg";
-  //mqtt.publish(topico.c_str(), msg.c_str());
-  //delay(1000);
-  //Serial.println(msg);
-  //mqtt.loop();
-
-  /*if (Serial.available() > 0) {
-    msg = Serial.readStringUntil('\n');
-    Serial.print("Mensagem digitada: ");
-    Serial.println(msg);
-    msg = "Beatriz: " + msg;
-    mqtt.publish("TopicoBruno", msg.c_str());  //Envia mensagem
-  } */
-
   luz = map(analogRead(pino_LDR), 0, 4095, 0, 100);
   char* = luz < 100 ? "claro" : "escuro";
   mqtt.publish(TOPIC_LUM, luz);
 
+  temperatura = sensorDHT.readTemperature(); 
+  mqtt.publish(TOPIC_TEMP, String(temperatura).c_str());
+  
+  umidade = sensorDHT.readHumidity();
+  mqtt.publish(TOPIC_UMID, String(umidade).c_str());
+
+  //DHT
+  Serial.print("Umidade: ");
+  Serial.print(umidade);
+  Serial.print("%");
+  Serial.println("Temperatura: ");
+  Serial.print(temperatura);
+  Serial.print("°C");
+
+  //presença
+  long distancia = lerDistancia();
+  
+  Serial.print("Distância: ");
+  Serial.print(distancia);
+  Serial.println(" cm");
+  
+  if (distancia < 10) {
+    mqtt.publish(TOPIC_PRESENCA,"Detectado");
+  }
+
   mqtt.loop();  // Mantem conexão
+  delay(500);
 }
+
 // Criação de função abaixo do loop
 void callback(char* topic, byte* payload, unsigned long length) {  //Topíco/ Mensagem / Tamanho da mensagem
   String msgRecebida = "";
@@ -72,15 +113,18 @@ void callback(char* topic, byte* payload, unsigned long length) {  //Topíco/ Me
     msgRecebida += (char)payload[i];
   }
   Serial.println(msgRecebida);
-  if (msgRecebida == "acender") {
-    digitalWrite(ledPin, HIGH);
-    delay(1000);
-    Serial.println("ligando...");
-  }
 
-  if (msgRecebida == "apagar") {
-    digitalWrite(ledPin, LOW);
-    delay(1000);
-    Serial.println("apagando...");
+  if (String(topic) == String(TOPIC_LUM)) {
+    if (msgRecebida == "escuro") {
+      digitalWrite(ledPin, HIGH);
+      delay(1000);
+      Serial.println("ligando...");
+    }
+
+    if (msgRecebida == "claro") {
+      digitalWrite(ledPin, LOW);
+      delay(1000);
+      Serial.println("apagando...");
+    }
   }
 }
